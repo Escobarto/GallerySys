3ca9f5e34afbb0089d03d8dba34115331e399439
Guilherme Escobar <125776546+Escobarto@users.noreply.github.com>
2025-12-16 09:36:33 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PublicGallery from './PublicGallery';
+import './index.css'; // Para garantir que o Tailwind funcione
+
+function App() {
+  return (
+    <div className="App">
+      <PublicGallery />
+    </div>
+  );
+}
+
+export default App;
